90db2fe1c208f4327a29ac195baead39d1465559
patrickbasham <PatrickTBasham@gmail.com>
2023-01-30 23:16:41 -0500

    added some file structure and base css reset file

---
+++ src/pages/UserForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserForm = () => {
+  return (
+    <div className="">
+      {/* need login / registration page */}
+    </div>
+  )
+}
+
+export default UserForm
