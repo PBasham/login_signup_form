34b4a43448fc8a4e94cda68fde60c310bebed2b9
patrickbasham <PatrickTBasham@gmail.com>
2023-02-01 14:12:32 -0500

    set up sudo code for user service functions for api calls

--- src/utilities/users-services.js
+++ src/utilities/users-services.js
@@ -3,12 +3,28 @@
 ========================================*/
 
 // check if email exist in database
+export async function checkEmail(userEmail) {
+    // { email: "", code_path: "CHECK_EMAIL"}
+    // check if an account in the database has that email address, if it doesn't, sends a verification code to the email
 
+}
 // check if verification code is correct
+export async function emailConfirmation(userData) {
+    // { email: "", code_path: "CHECK_CODE", verification_code: ""}
+    // Checks if a verificaton code is valid
+}
 
 // register the user
+export async function registerUser(userData) {
+    // {email: "", full_name: "", new_password: "", new_hash: "", verification_code: "", source_of_business: "unknown", ""additional_info_for_source: "" }
+    // Creates a new user account
+}
 
 // verify & login the user
+export async function login(userCredentials) {
+    // {email: "", password: ""}
+    // verify user credentials and returns a user objext and session ID
+}
 
 
 // user-services helper functions --------------------------------------------------
