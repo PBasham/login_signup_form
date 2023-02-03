0b191748503d26ce81a4b120971983421706c300
patrickbasham <PatrickTBasham@gmail.com>
2023-02-03 16:14:54 -0500

    updated some styling for signup form

--- src/components/Auth/SignUpForm.jsx
+++ src/components/Auth/SignUpForm.jsx
@@ -171,7 +171,7 @@ const SignUpForm = (props) => {
                             disabled={disableBtn}
                         >Create Account
                         </button>
-                        <p className="error-message">{errorMsg}</p>
+                        <p className="error-message error-text">{errorMsg}</p>
                     </form>
                     <div className="alt-form-div">
                         <p>Already signed up? <span className="auth-form-link" onClick={updateShowLogin}>Log In</span></p>
@@ -182,9 +182,20 @@ const SignUpForm = (props) => {
                 <div className="verification-div">
                     <h1>A verification code has been sent to your email.</h1>
                     <form autoComplete="off" onSubmit={checkVerificationCode} >
-                        <input className={`form-input ${verCodeError ? "input-error" : null}`} type="text" name="verification_code" placeholder="Verification Code" value={credentials.verification_code} onChange={handleFormOnChange} required />
+                        <input
+                            className={`form-input ${verCodeError ? "input-error" : null}`}
+                            type="text"
+                            name="verification_code"
+                            placeholder="Verification Code"
+                            value={credentials.verification_code}
+                            onChange={handleFormOnChange}
+                            required />
                         <p className="error-message">{errorMsg}</p>
-                        <button type="submit" className={`btn auth-btn btn-v-two ${disableBtn ? "disabledBtn" : null}`} disabled={disableBtn} >Continue</button>
+                        <button
+                            type="submit"
+                            className={`btn auth-btn btn-v-two ${disableBtn ? "disabledBtn" : null}`}
+                            disabled={disableBtn}
+                        >Continue</button>
                         <p>{generalMsg}</p>
                         <p>Didn't recieve the code? <span className="auth-form-link" onClick={resendVerificationCode} >Resend code</span></p>
                     </form>
